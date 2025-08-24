2aa4b615b852cac917c97b397f51836db71357d5
Kebron Gizachew <kebisel21@gmail.com>
2025-08-24 19:23:38 +0300

    working on my dialog html file with css styling

---
+++ script.js
@@ -0,0 +1,14 @@
+const xoro=document.getElementById('xoro');
+const board=document.querySelectorAll('#board div');
+const choice=document.getElementById('choice');
+const computer=document.getElementById('computer');
+const friend=document.getElementById('other');
+const enter=document.getElementById('enter');
+const player1=document.getElementById('player1');
+const player2=document.getElementById('player2');
+const start=document.getElementById('start');
+const buttonx=document.getElementById('x');
+const buttono=document.getElementById('o');
+const reset=document.getElementById('reset');
+const buttonsure=document.getElementById('sure');
+xoro.showModal()
